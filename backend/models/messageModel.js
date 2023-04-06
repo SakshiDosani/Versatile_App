64a487a75bc2e28e3a9e750db372860d4574b763
Sakshi <sakshi.dosani04@gmail.com>
2023-04-06 14:10:35 +0530

    Project Added

---
+++ backend/models/messageModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const messageSchema = Schema(
+  {
+    sender: { type: Schema.Types.ObjectId, ref: "User" },
+    content: { type: String, trim: true },
+    chat: { type: Schema.Types.ObjectId, ref: "Chat" },
+    readBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
+
+const Message = model("Message", messageSchema);
+module.exports = Message;
